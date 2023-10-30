348e243dd7a89372fff9b6be3c04b05dca5e9779
gonzaloonofreHV <108018545+gonzaloonofreHV@users.noreply.github.com>
2023-10-30 11:27:50 +0100

    Update superset_config_ex.py
    Cambio a idioma por defecto inglés

--- superset_config_ex.py
+++ superset_config_ex.py
@@ -1,9 +1,9 @@
 # Setup default language
-BABEL_DEFAULT_LOCALE = 'zh'
+BABEL_DEFAULT_LOCALE = 'en'
 # Your application default translation path
 BABEL_DEFAULT_FOLDER = 'superset/translations'
 # The allowed translation for you app
 LANGUAGES = {
     'en': {'flag': 'us', 'name': 'English'},
-    'zh': {'flag': 'cn', 'name': 'Chinese'},
+   
 }
