6677804ba0b4c29514d956cf2de59346484c16d6
gonzaloonofreHV <108018545+gonzaloonofreHV@users.noreply.github.com>
2023-11-03 12:10:26 +0100

    Update superset_config_ex.py
    Añadido español

--- superset_config_ex.py
+++ superset_config_ex.py
@@ -1,9 +1,10 @@
 # Setup default language
-BABEL_DEFAULT_LOCALE = 'en'
+BABEL_DEFAULT_LOCALE = 'es'
 # Your application default translation path
 BABEL_DEFAULT_FOLDER = 'superset/translations'
 # The allowed translation for you app
 LANGUAGES = {
     'en': {'flag': 'us', 'name': 'English'},
+    'es': {'flag': 'es', 'name': 'Español'},
    
 }
